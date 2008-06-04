b0f483079758f64b6425406a72fedc27ddccc2a8
tinorj@gmail.com <WOC@.(none)>
2008-06-04 09:50:11 -0300

    colocando os arquivos na pasta certa

---
+++ for_windows/model.bat
@@ -0,0 +1,3 @@
+@REM Atalho para execução do generate model de aplicações Rails
+@REM Script desenvolvido por Celestino Gomes (http://tinogomes.wordpress.com/)
+ruby script/generate model %*
---
+++ for_windows/scaffold.bat
@@ -0,0 +1,3 @@
+@REM Atalho para execução do generate scaffold de aplicações Rails
+@REM Script desenvolvido por Celestino Gomes (http://tinogomes.wordpress.com/)
+ruby script/generate scaffold %*
